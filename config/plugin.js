21ff5f0328b54642faca0dff946c9b8354f6533f
lianfangti <lianfangti@qq.com>
2019-06-19 16:00:39 +0800

    关闭 egg-view-assets

--- config/plugin.js
+++ config/plugin.js
@@ -18,8 +18,8 @@ module.exports = {
     enable: true,
     package: 'egg-view-nunjucks',
   },
-  assets : {
-    enable: true,
-    package: 'egg-view-assets',
-  }
+  // assets : {
+  //   enable: true,
+  //   package: 'egg-view-assets',
+  // }
 };
