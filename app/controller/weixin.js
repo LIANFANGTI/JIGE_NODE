586a8a10725bdfe444d41426d353acda1ba97057
lianfangti <lianfangti@qq.com>
2019-06-21 00:19:23 +0800

    领取后扣去商户余额调试

--- app/controller/weixin.js
+++ app/controller/weixin.js
@@ -320,7 +320,7 @@ module.exports = class WeixinController extends BaseController {
                         console.log(`调试:调用Eleme接口返回值`, res);
                         if (res.code === 1) {
                             await ctx.service.user.update({times: user.times - ctx.mpconfig.unit_coin}, {openid});
-                            await ctx.service.mpconfig.update({blance: Sequelize.literal(`blance - unit_price`)});//减去账户余额测试
+                            await ctx.service.mpconfig.update({blance: Sequelize.literal(`blance - ${this.ctx.mpconfig.unit_price}`)});//减去账户余额测试
                             let log = {
                                 uid: user.id,
                                 times: user.times - this.ctx.mpconfig.unit_coin,
